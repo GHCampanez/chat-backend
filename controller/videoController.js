09435acfdccb8bdee5d0c82cd82fc9c2b2cd6970
Guilherme Campanez <guilhermecampanez@gmail.com>
2019-12-06 10:25:44 -0300

    video stream

--- controller/videoController.js
+++ controller/videoController.js
@@ -7,15 +7,32 @@ router.get('/', function (req, res) {
 
   const path = req.query.videoPath
 
-  const stat = fs.statSync(path)
-  const fileSize = stat.size
-
-  const head = {
-    'Content-Length': fileSize,
-    'Content-Type': 'video/mp4',
-  }
-  res.writeHead(200, head)
-  fs.createReadStream(path).pipe(res)
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.log(err);
+      return res.status(404).end('<h1>Movie Not found</h1>');
+    }
+    // Variáveis necessárias para montar o chunk header corretamente
+    const { range } = req.headers;
+    const { size } = stats;
+    const start = Number((range || '').replace(/bytes=/, '').split('-')[0]);
+    const end = size - 1;
+    const chunkSize = (end - start) + 1;
+    // Definindo headers de chunk
+    res.set({
+      'Content-Range': `bytes ${start}-${end}/${size}`,
+      'Accept-Ranges': 'bytes',
+      'Content-Length': chunkSize,
+      'Content-Type': 'video/mp4'
+    });
+    // É importante usar status 206 - Partial Content para o streaming funcionar
+    res.status(206);
+    // Utilizando ReadStream do Node.js
+    // Ele vai ler um arquivo e enviá-lo em partes via stream.pipe()
+    const stream = fs.createReadStream(path, { start, end });
+    stream.on('open', () => stream.pipe(res));
+    stream.on('error', (streamErr) => res.end(streamErr));
+  });
 
 })
 
